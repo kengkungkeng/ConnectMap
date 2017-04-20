434e2e9993f032e07d951b1baddbb1d0b621f963
kengkungkeng <kengkungkeng@gmail.com>
2017-04-20 11:50:17 +0700

    Initial commit

---
+++ app/src/main/java/comsci/kengkung/connectmap/CallMap.java
@@ -0,0 +1,34 @@
+package comsci.kengkung.connectmap;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.net.URL;
+
+public class CallMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_map);
+    }
+
+    public void runcallMap(View view){
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText label = (EditText) findViewById(R.id.mapLabel);
+
+        String lat=latitude.getText().toString().trim();
+        String lng=longtitude.getText().toString().trim();
+        String mlabel=label.getText().toString().trim();
+
+        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.759196,100.541312");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        //mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+}
